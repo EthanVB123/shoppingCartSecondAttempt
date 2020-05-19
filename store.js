bb326db73ed0dd4a3782a38f4e43ded971c47343
EthanVB123 <ethanvanbruchem@gmail.com>
2020-05-19 10:28:40 +0800

    Commit

--- store.js
+++ store.js
@@ -1,18 +1,53 @@
+// What follows is the initialisation of the remove buttons
+updateCartTotal()
 var removeCartItemButtons = document.getElementsByClassName('cartremovebtn')
 for (var j = 0; j < removeCartItemButtons.length; j++) {
     var button = removeCartItemButtons[j]
-    console.log(button)
     button.addEventListener('click', function(event) {
         var buttonClicked = event.target
         buttonClicked.parentElement.parentElement.remove()
         updateCartTotal()
     })
 }
+// What follows is the initialisation of the quantity inputs
+for (var i = 0; i < quantityInputs.length; i++) {
+    var input = quantityInputs[i]
+    input.addEventListener('change', function(event) {
+        var input = event.target
+        if (isNaN(input.value) || input.value <= 0) {
+            input.value = 1
+        }
+        updateCartTotal()
+    }
+}
 
 function updateCartTotal() {
     var cartItemContainer = document.getElementsByClassName('cart-items')[0]
     var cartRows = cartItemContainer.getElementsByClassName('cart-row')
-    for (var i = 0, i < cartRows.length)
+    var total = 0
+    for (var i = 0; i < cartRows.length; i++) {
+        var cartRow = cartRows[i]
+        var priceElement = cartRow.getElementsByClassName('cart-price')[0]
+        var quantityElement = cartRow.getElementsByClassName('cart-quantity-input')[0]
+        console.log(priceElement, quantityElement)
+        var price = parseFloat(priceElement.innerText.replace('$', ''))
+        console.log(price)
+        var quantity = quantityElement.value
+        console.log(price * quantity)
+        total += (price*quantity)
+
+    }
+    total = Math.round(total * 100)/100
+    document.getElementsByClassName('cart-total-price')[0].innerText = '$' + total
 
 
+}
+
+function quantityChanged(event) {
+    var input = event.target
+    if (isNaN(input.value) || input.value <= 0) {
+        input.value = 1
+    }
+    updateCartTotal()
+    
 }
